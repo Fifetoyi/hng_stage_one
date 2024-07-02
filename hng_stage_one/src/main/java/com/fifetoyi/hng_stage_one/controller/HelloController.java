b5b1dbb91dd5f1be716159e0610373dff9ae44e3
Fifetoyi <fifetoyi@gmail.com>
2024-07-02 17:06:09 +0100

    Add x-forwarded

--- hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/controller/HelloController.java
+++ hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -23,16 +24,26 @@ public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/api/hello")
-    public Map<String, String> hello(@RequestParam String visitor_name) {
-        Map<String, String> locationInfo = locationService.getLocation();
-        String clientIp = locationInfo.get("ip");
-        String city = locationInfo.get("city");
+    public Map<String, String> hello(@RequestParam String visitor_name, HttpServletRequest request) {
+        String clientIp = getClientIp(request);
         log.info("The client IP: " + clientIp);
+
+        Map<String, String> locationInfo = locationService.getLocation(clientIp);
+        String city = locationInfo.get("city");
+
         String temperature = weatherService.getTemperature(city);
         return Map.of(
                 "client_ip", clientIp,
                 "location", city,
-                "greeting", String.format("Hello, %s!, The temperature is %s degrees Celsius in %s", visitor_name, temperature, city)
+                "greeting", String.format("Hello, %s! The temperature is %s degrees Celsius in %s", visitor_name, temperature, city)
         );
+    }
+
+    private String getClientIp(HttpServletRequest request) {
+        String remoteAddr = request.getHeader("X-FORWARDED-FOR");
+        if (remoteAddr == null || remoteAddr.isEmpty()) {
+            remoteAddr = request.getRemoteAddr();
+        }
+        return remoteAddr;
     }
 }
--- hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/service/LocationService.java
+++ hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/service/LocationService.java
@@ -1,6 +1,5 @@
 package com.fifetoyi.hng_stage_one.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
@@ -14,11 +13,10 @@ public class LocationService {
     private final RestTemplate restTemplate = new RestTemplate();
     private static final Logger log = LoggerFactory.getLogger(LocationService.class);
 
-    public Map<String, String> getLocation() {
-        String url = "http://ip-api.com/json/";
+    public Map<String, String> getLocation(String ip) {
+        String url = "http://ip-api.com/json/" + ip;
         Map<String, Object> response = restTemplate.getForObject(url, Map.class);
         log.info("Location API response: " + response);
-        String ip = (String) response.get("query");
         String city = (String) response.get("city");
         String region = (String) response.get("regionName");
         String country = (String) response.get("country");
