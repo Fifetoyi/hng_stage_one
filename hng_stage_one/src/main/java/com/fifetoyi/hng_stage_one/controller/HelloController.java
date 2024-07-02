faa1639cafd99c41218957fe2bfdfa526fe0e4ba
Fifetoyi <fifetoyi@gmail.com>
2024-07-02 15:04:53 +0100

    Refactor code to get client ip

--- hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/controller/HelloController.java
+++ hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/controller/HelloController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -19,15 +20,19 @@ public class HelloController {
     @Autowired
     private WeatherService weatherService;
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/api/hello")
-    public Map<String, String> hello(@RequestParam String visitor_name, HttpServletRequest request) {
-        String clientIp = request.getRemoteAddr();
-        Map<String, String> location = locationService.getLocation(clientIp);
-        String temperature = weatherService.getTemperature(location.get("city"));
+    public Map<String, String> hello(@RequestParam String visitor_name) {
+        Map<String, String> locationInfo = locationService.getLocation();
+        String clientIp = locationInfo.get("ip");
+        String city = locationInfo.get("city");
+        log.info("The client IP: " + clientIp);
+        String temperature = weatherService.getTemperature(city);
         return Map.of(
                 "client_ip", clientIp,
-                "location", location.get("city"),
-                "greeting", String.format("Hello, %s!, The temperature is %s degrees Celsius in %s", visitor_name, temperature, location.get("city"))
+                "location", city,
+                "greeting", String.format("Hello, %s!, The temperature is %s degrees Celsius in %s", visitor_name, temperature, city)
         );
     }
 }
--- hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/service/LocationService.java
+++ hng_stage_one/src/main/java/com/fifetoyi/hng_stage_one/service/LocationService.java
@@ -3,23 +3,25 @@ package com.fifetoyi.hng_stage_one.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 @Service
 public class LocationService {
 
-    @Value("${weatherapi.api.key}")
-    private String weatherApiKey;
-
     private final RestTemplate restTemplate = new RestTemplate();
+    private static final Logger log = LoggerFactory.getLogger(LocationService.class);
 
-    public Map<String, String> getLocation(String ip) {
-        String url = "http://api.weatherapi.com/v1/ip.json?key=" + weatherApiKey + "&q=auto:ip";
+    public Map<String, String> getLocation() {
+        String url = "http://ip-api.com/json/";
         Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+        log.info("Location API response: " + response);
+        String ip = (String) response.get("query");
         String city = (String) response.get("city");
-        String region = (String) response.get("region");
-        String country = (String) response.get("country_name");
-        return Map.of("city", city, "region", region, "country", country);
+        String region = (String) response.get("regionName");
+        String country = (String) response.get("country");
+        return Map.of("ip", ip, "city", city, "region", region, "country", country);
     }
 }
